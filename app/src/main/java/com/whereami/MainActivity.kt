625a010ce98e114579a2eafa589db8dfa4ea212d
sysout <sys.yugulis@gmail.com>
2026-08-12 23:29:07 -0300

    ui: add local Fredoka and Nunito fonts and set Nunito as default typography

--- app/src/main/java/com/whereami/MainActivity.kt
+++ app/src/main/java/com/whereami/MainActivity.kt
@@ -3,8 +3,8 @@ package com.whereami
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
 import com.whereami.presentation.navigation.AppNavigation
+import com.whereami.presentation.theme.WhereAmITheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
+            WhereAmITheme {
                 AppNavigation()
             }
         }
---
+++ app/src/main/java/com/whereami/presentation/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.whereami.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PinRed = Color(0xFFE53935)
+val AccentYellow = Color(0xFFFFB300)
+val EarthGreen = Color(0xFF7CB342)
+val OceanBlue = Color(0xFF1E88E5)
+val SkyTop = Color(0xFF81D4FA)
+val SkyBase = Color(0xFF29B6F6)
+val White = Color(0xFFFFFFFF)
+val DarkBlue = Color(0xFF0D47A1)
+val SoftBlue = Color(0xFF1565C0)
+val SurfaceBlue = Color(0xFFE3F2FD)
